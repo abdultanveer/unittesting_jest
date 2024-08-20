554ed1192db3a990037a3f35b552cf3c7c9ddfe8
abdul <abdul.tanveer@gmail.com>
2024-08-20 15:54:09 +0530

    enter 10 in the input field

--- react-rtl/react-rtl/src/components/counter/counter.test.tsx
+++ react-rtl/react-rtl/src/components/counter/counter.test.tsx
@@ -33,5 +33,15 @@ describe('counter',()=>{
 //tripleclick,hover,unhover --  convinience api's
      })
 
+     test('sets 10 after clicking set button',async()=>{
+        user.setup()
+        render(<Counter/>)
+        const amountInput = screen.getByRole('spinbutton')
+       await user.type(amountInput,'10')
+        expect(amountInput).toHaveValue(10)
+
+
+     })
+
 
 })
